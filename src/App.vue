a5e0077b501500f3bc7dacbc2ca15b1bb13e6778
Juan Melo <jcmelop@ufpso.edu.co>
2023-05-25 10:14:29 -0500

    agregue prevent al metodo onclick cuando para que cuando se da click derecho no se desplegue el menu

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <script setup>
 const msg = 'Vue 3 eventos'
 const onclick = (msg) =>{
-      alert(msg)
+      console.log(msg)
 }    
 </script>
 
 <template>
  <h1>{{ msg }}</h1>
- <button @click.right="onclick('me has dado click con el boton derecho')">Dar click derecho</button>
+ <button @click.right.prevent="onclick('me has dado click con el boton derecho')">Dar click derecho</button>
  <button @click.left="onclick('me has dado click con el boton izquierdo')">Dar click izquierdo</button>
 </template>
 
