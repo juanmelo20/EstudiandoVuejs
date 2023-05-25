543a179a90ae93d3182e3698ca4975e8354d6ff1
Juan Melo <jcmelop@ufpso.edu.co>
2023-05-25 09:42:43 -0500

    eventos VueJS

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+const msg = 'Vue 3 eventos'
+const onclick = () =>{
+      alert('me has pinchado')
+}    
+</script>
+
+<template>
+ <h1>{{ msg }}</h1>
+ <button @click="onclick">Dar click</button>
+</template>
+
+<style scoped>
+
+</style>
